945500fa7d9e17e38e782d2c102880738bd0982d
sa-school <146558348+sa-school@users.noreply.github.com>
2026-03-15 16:10:51 -0400

    Fixed DashboardControl

--- FirstProject/dashboardcontrol.cpp
+++ FirstProject/dashboardcontrol.cpp
@@ -1,2 +1,21 @@
 #include "dashboardcontrol.h"
 
+DashboardControl::DashboardControl(Vendor* v) : vendor(v) {}
+
+int DashboardControl::getCategory() { return vendor->getCategory(); }
+BusinessInfo DashboardControl::getBusinessInfo() { return vendor->getBusinessInfo(); }
+BusinessLicence DashboardControl::getBusinessLicence() { return vendor->getBusinessLicence(); }
+Insurance DashboardControl::getLiabilityInsurance() { return vendor->getLiabilityInsurance(); }
+Certification DashboardControl::getFoodHanderCert() { return vendor->getFoodHandlerCert(); }
+
+bool DashboardControl::hasBookedDates() {
+    if (!(vendor->getBookedDates().empty())) {
+        return true;
+    }
+
+    return false;
+}
+
+std::vector<MarketDate*> DashboardControl::getBookedDates() {
+    return vendor->getBookedDates();
+}
--- FirstProject/dashboardcontrol.h
+++ FirstProject/dashboardcontrol.h
@@ -5,7 +5,28 @@
 #include "vendor.h"
 
 class DashboardControl {
+    public:
+        DashboardControl(Vendor* v);
 
+        // vendor info
+        int getCategory();
+        BusinessInfo getBusinessInfo();
+        BusinessLicence getBusinessLicence();
+        Insurance getLiabilityInsurance();
+        Certification getFoodHanderCert();
+
+        // stall bookings
+        bool hasBookedDates();
+        std::vector<MarketDate*> getBookedDates();
+
+        // waitlists
+
+        // confirmation messages
+
+        // alerts
+
+    private:
+        Vendor* vendor;
 };
 
 #endif // DASHBOARDCONTROL_H
